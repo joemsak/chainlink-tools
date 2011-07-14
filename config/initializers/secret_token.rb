149202977e8b32c77d080ad0a71a0985820a52f8
Joe Sak <joe@joesak.com>
2011-07-13 20:21:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clinventory::Application.config.secret_token = '74ee918d3af5c8df94bc81dc97e565d158eddb18d6c0b53dbe0529a7a2ef48bcbbcaa29bf72a19a4aaa34729b6f94292e71d78859a115072f40f31f2f236eb57'
