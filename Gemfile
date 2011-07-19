097abe71683e9d1e6e36122cc69f0a0be3d418d0
Joe Sak <joe@joesak.com>
2011-07-18 20:12:56 -0500

    shit i need

--- Gemfile
+++ Gemfile
@@ -2,11 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0.rc4'
 
-group :development do
-  gem 'heroku'
-  gem 'taps'
-end
-
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
@@ -18,6 +13,8 @@ gem 'coffee-script'
 gem 'uglifier'
 
 gem 'jquery-rails'
+
+gem 'simple_form'
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -31,4 +28,26 @@ gem 'jquery-rails'
 group :test do
   # Pretty printed test output
   gem 'turn', :require => false
+end
+
+group :test, :development do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-cucumber'
+  
+  gem 'database_cleaner'
+  gem 'rails3-generators' #mainly for factory_girl & simple_form at this point
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'cucumber-rails'
+  gem 'capybara'
+end
+
+group :development do
+  gem 'heroku'
+  gem 'taps'
+end
+
+group :production do
+  gem 'therubyracer-heroku', '0.8.1.pre3'
 end
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Clinventory
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -35,6 +35,15 @@ module Clinventory
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.test_framework :rspec, :views => false, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.form_builder :simple_form
+    end
+    
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Enable the asset pipeline
     config.assets.enabled = true
