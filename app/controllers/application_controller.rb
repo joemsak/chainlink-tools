e888a0ac175b1667f5c90d21a64c088708ea65de
Joe Sak <joe@joesak.com>
2011-08-26 08:52:46 -0500

    update username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => "julie", :password => "chainlink" if Rails.env == 'production'
+  http_basic_authenticate_with :name => "juliehochstadter", :password => "chainlink" if Rails.env == 'production'
   
 end
